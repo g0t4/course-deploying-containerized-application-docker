97d57d4002fab65f27b856cc9abd159b3c1d894d
Wes Higbee <wes.mcclure@gmail.com>
2025-01-28 03:52:21 -0600

    buggy

--- pip/app/main.py
+++ pip/app/main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
+@app.route("/adder/<string:a>/<string:b>")
+def adder(a, b):
+    a_float = float(a)
+    b_float = float(b)
+    return a_float + b_float
 
 @app.route("/")
 def home():
@@ -9,4 +14,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000)
